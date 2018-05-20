ab0c3bd5c418e4e3b2c863e2fba374f3c631a335
ul7r41337h4x0r <39104323+ul7r41337h4x0r@users.noreply.github.com>
2018-05-20 01:36:05 -0700

    Update program.py

--- program.py
+++ program.py
@@ -1,23 +1,22 @@
-#import random
 import random
 
-print('------------------------------------')
-print('         Guess The Number Game')
-print('------------------------------------')
-print()
-# alt + enter will try to autofix with red bulb in pycharm
-the_number = random.randint(0,100)
-guess = -1
+def intro ():
+    print('------------------------------------')
+    print('         Guess The Number Game')
+    print('------------------------------------')
+    print('Guess a number between 0 and 100:')
 
 
-while guess != the_number:
-    guess_text = input('Guess a number between 0 and 100: ')
-    # conver to an integer
+the_number = random.randint(0, 100)
+intro()
+
+while True:
+    guess_text = input()
     guess = int(guess_text)
-
     if guess < the_number:
-        print("Too low")
+        print(guess_text, "is too low")
     elif guess > the_number:
-        print("Too High")
+        print(guess_text, "is too High")
     else:
         print('you win!')
+        exit(0)
