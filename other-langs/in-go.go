1e3793890c8aa3fde50441e766cc76eda82331ae
Iyler <39569566+Iyler@users.noreply.github.com>
2018-05-23 14:24:35 -0700

    Update in-go.go

--- other-langs/in-go.go
+++ other-langs/in-go.go
@@ -8,8 +8,12 @@ import (
 	"strconv"
 )
 
+var (
+	highNumber int64 = 100
+)
+
 func randomNumber() int64 {
-	res, _ := rand.Int(rand.Reader, big.NewInt(100))
+	res, _ := rand.Int(rand.Reader, big.NewInt(highNumber))
 	return res.Int64()
 
 }
@@ -18,7 +22,7 @@ func intro() {
 	fmt.Println("------------------------------------")
 	fmt.Println("       Guess The Number Game        ")
 	fmt.Println("------------------------------------")
-	fmt.Println("Guess a number between 0 and 100:")
+	fmt.Printf("Guess a number between 0 and %d:\n", highNumber)
 }
 
 func userInput() int64 {
@@ -45,5 +49,4 @@ func main() {
 			os.Exit(0)
 		}
 	}
-
 }
